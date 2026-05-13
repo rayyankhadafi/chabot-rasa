35d4058abfcfe1f0e37cbbc145057c69adfa2559
Rayyan <riankhadafi21@gmail.com>
2026-05-13 17:36:46 +0700

    fix railway host binding

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
