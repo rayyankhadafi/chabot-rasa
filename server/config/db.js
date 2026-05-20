24ba1c86d24c47445e1978bd8ede677dda2733e6
rayyankhadafi <riankhadafi21@gmail.com>
2026-05-21 05:35:50 +0700

    fix mysql pool

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,24 @@
 const mysql = require("mysql2");
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 60000,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.log("Database connection failed:", err);
+    console.error("Database connection failed:", err);
   } else {
     console.log("MySQL Connected");
+    connection.release();
   }
 });
 
