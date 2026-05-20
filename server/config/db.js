5c3ec31235f7e693b04ded0e4b4c4c8668762f7d
rayyankhadafi <riankhadafi21@gmail.com>
2026-05-21 03:47:21 +0700

    debug env

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,10 @@
 const mysql = require("mysql2");
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
